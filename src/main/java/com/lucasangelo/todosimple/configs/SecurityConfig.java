7f4b33635ce2405b8feae88f1ccc5a852b731ea9
Nicolas Frech <nicfrech@gmail.com>
2024-06-19 17:54:25 -0300

    Create UserDetailsServiceImp and JWTUtil

--- src/main/java/com/lucasangelo/todosimple/configs/SecurityConfig.java
+++ src/main/java/com/lucasangelo/todosimple/configs/SecurityConfig.java
@@ -2,13 +2,17 @@ package com.lucasangelo.todosimple.configs;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -19,6 +23,11 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
     
     private static final String[] PUBLIC_MATCHES = {
         "/"
@@ -33,6 +42,14 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         
         http.cors().and().csrf().disable();
+
+        AuthenticationManagerBuilder authenticationManagerBuilder = http
+            .getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(this.userDetailsService)
+            .passwordEncoder(bCryptPasswordEncoder());
+        this.authenticationManager = authenticationManagerBuilder.build();
+
+
 
         http.authorizeRequests()
             .antMatchers(HttpMethod.POST, PUBLIC_MATCHES_POST).permitAll()
--- src/main/java/com/lucasangelo/todosimple/repositories/UserRepository.java
+++ src/main/java/com/lucasangelo/todosimple/repositories/UserRepository.java
@@ -2,6 +2,7 @@ package com.lucasangelo.todosimple.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.lucasangelo.todosimple.models.User;
 
@@ -10,5 +11,7 @@ import com.lucasangelo.todosimple.models.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Transactional(readOnly = true)
+    User findByUsername(String username);
     
 }
